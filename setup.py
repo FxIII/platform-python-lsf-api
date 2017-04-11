64e0f1b013edb64bb10075889681518b1b4ffb04
FxIII <FxIII@users.noreply.github.com>
2017-04-11 14:38:36 +0200

    update setup ( tools and includes)

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+# !/usr/bin/env python
 
 # 
 # (C) Copyright IBM Corporation 2013
@@ -7,8 +7,12 @@
 # modify it under the terms of the Eclipse Public License.
 # 
 import os
-from distutils.core import setup, Extension
-setup(name='platform-python-lsf-api',
+import sys
+from setuptools import setup, Extension
+
+includeDir = os.path.join(sys.prefix, 'include')
+includeDirs = [i for i in (os.path.join(includeDir, i) for i in os.listdir(includeDir)) if os.path.isdir(i)] + [includeDir]
+setup(name='pythonlsf',
       version='1.0.1',
       description='Python binding for Platform LSF APIs',
       license='LGPL',
@@ -16,23 +20,22 @@ setup(name='platform-python-lsf-api',
       url='http://www.platform.com',
       ext_package='pythonlsf',
       ext_modules=[Extension('_lsf', ['pythonlsf/lsf.i'],
-                               include_dirs=['/usr/include/python2.4'],
-                               library_dirs=[os.environ['LSF_LIBDIR']],
-                               swig_opts=['-I' + os.environ['LSF_LIBDIR'] + '/../../include/lsf/'],
-                               extra_compile_args=['-m64', '-I' + os.environ['LSF_LIBDIR'] + '/../../include/lsf/'],			
-                               extra_link_args=['-m64'],
-                               libraries=['c', 'nsl', 'lsbstream', 'lsf', 'bat', 'rt',
-                                            'fairshareadjust'])],
+                             include_dirs=includeDirs,
+                             library_dirs=[os.environ['LSF_LIBDIR']],
+                             swig_opts=['-I' + os.environ['LSF_LIBDIR'] + '/../../include/lsf/'],
+                             extra_compile_args=['-m64', '-I' + os.environ['LSF_LIBDIR'] + '/../../include/lsf/'],
+                             extra_link_args=['-m64'],
+                             libraries=['c', 'nsl', 'lsbstream', 'lsf', 'bat', 'rt',
+                                        'fairshareadjust'])],
       py_modules=['pythonlsf.lsf'],
       classifiers=["Development Status :: 2 - Pre-Alpha",
-                     "License :: OSI Approved :: Eclipse Public License",
-                     "Operating System :: OS Independent",
-                     "Programming Language :: Python",
-                     "Topic :: Internet",
-                     "Topic :: Scientific/Engineering",
-                     "Topic :: Software Development",
-                     "Topic :: System :: Distributed Computing",
-                     "Topic :: Utilities",
-                     ],
-     )
-
+                   "License :: OSI Approved :: Eclipse Public License",
+                   "Operating System :: OS Independent",
+                   "Programming Language :: Python",
+                   "Topic :: Internet",
+                   "Topic :: Scientific/Engineering",
+                   "Topic :: Software Development",
+                   "Topic :: System :: Distributed Computing",
+                   "Topic :: Utilities",
+                   ],
+      )
